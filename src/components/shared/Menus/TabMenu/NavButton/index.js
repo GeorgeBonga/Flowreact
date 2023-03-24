2f5be19bfe31eb7c69a0b91dde2e248e71d04919
George Bonga <bongageorge18@gmail.com>
2023-03-24 15:08:18 +0300

    new comit

---
+++ src/components/shared/Menus/TabMenu/NavButton/index.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import { MaterialIcons, FontAwesome } from '@expo/vector-icons'; 
+
+// import * as S from './styles'
+
+// export default function NavButton (props) {
+//   return (
+//     <S.Column onPress={props.onPress}>
+//       {/* <FontAwesome name={props.icon} size={24} color="#44C2E4" /> */}
+//       <S.Text>{props.text}</S.Text>
+//     </S.Column>
+    
+//   )
+// }
